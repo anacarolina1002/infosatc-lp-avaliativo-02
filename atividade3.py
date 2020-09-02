5280ac1bc73bdb189daa2d7275e3709e91839d86
Anacarolina1002 <carolquaglioto@outlook.com>
2020-09-02 17:50:02 -0300

    :alien: feat: exercicio

---
+++ atividade3.py
@@ -0,0 +1,11 @@
+#Escreva um programa para verificar se um elemento x está presente em uma lista.
+
+listaA = ["Python","Java","PHP",]
+print(listaA)
+
+print("Python está na lista?")
+
+if "Python" in listaA:
+    print("Sim, Python está na lista!")
+else:
+    print("Não, Python não está na lista!")
