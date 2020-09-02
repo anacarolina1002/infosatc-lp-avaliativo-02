832e7e54e138b6c68b74b6dfb10b56b40ac70adb
Anacarolina1002 <carolquaglioto@outlook.com>
2020-09-02 18:52:06 -0300

    :alien: feat: exercicio

---
+++ atividade6.py
@@ -0,0 +1,9 @@
+#Faça uma lista de números inteiros em ordem aleatória 
+#e mostre ao usuário a lista ordenada.
+
+listaAleatoria = [2,5,4,3,1,7,9,6,8,10]
+listaOrdenada = sorted(listaAleatoria)
+
+print ("A lista original é:",listaAleatoria)
+print ("A lista ordenada é:",listaOrdenada)
+
